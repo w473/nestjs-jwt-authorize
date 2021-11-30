91efdaa379d7a08303787c7cd959b26b2d4ef4e9
Jacek Głogosz <jacek.glogosz@gmail.com>
2021-11-30 20:35:14 +0100

    added index

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './decorators/has-role.decorator';
+export * from './decorators/public.decorator';
+export * from './models/user';
+export * from './models/keycloak.jwt.token';
+export * from './authorization.guard';
+export * from './roles.guard';
