46e56244129997ecc53723f1ddb3cf495c770b55
Jacek Głogosz <jacek.glogosz@gmail.com>
2021-12-08 20:30:42 +0100

    fix build

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './decorators/has-role.decorator';
 export * from './models/user';
 export * from './models/keycloak.jwt.token';
 export * from './roles.guard';
+export * from './middlewares/authentication.middleware';
