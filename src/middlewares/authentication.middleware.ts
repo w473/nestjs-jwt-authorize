21056bc9ee2bf10cb1a54410d0eb0a2087426d11
Jacek Głogosz <jacek.glogosz@gmail.com>
2022-01-20 21:47:06 +0100

    feat(logger) Logged added to AuthenticationMiddleware verbose level

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -1,6 +1,7 @@
 import {
   Inject,
   Injectable,
+  Logger,
   NestMiddleware,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -10,6 +11,8 @@ export const AUTHORIZATION_HEADER_NAME = 'AUTHORIZATION_HEADER_NAME';
 
 @Injectable()
 export class AuthenticationMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(AuthenticationMiddleware.name);
+
   constructor(
     @Inject(AUTHORIZATION_HEADER_NAME) private authorizationHeader: string,
     @Inject('TokenParserInterface')
@@ -25,10 +28,15 @@ export class AuthenticationMiddleware implements NestMiddleware {
       const token = req.header(this.authorizationHeader);
       if (token) {
         const jwt = JSON.parse(Buffer.from(token, 'base64').toString());
+        this.logger.verbose('Received JWT', jwt);
         req.user = this.tokenParser.getUserFromTokenObject(jwt);
+        this.logger.verbose('User parsed from JWT', req.user);
         return next();
       }
-    } catch (error) {}
+    } catch (error) {
+      this.logger.verbose('Fault parsing JWT', error);
+    }
+    this.logger.verbose('Token does not exists');
     throw new UnauthorizedException();
   }
 }
